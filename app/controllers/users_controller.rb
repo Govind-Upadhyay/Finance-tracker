ca46157ea97379b69163cd23b89afee780e8b79c
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-05 15:40:32 +0530

    Add search friends form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,26 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+  	if params[:friend].present?
+      @friend = friend.new_lookup(params[:friend])
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end    
+    else
+       respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
+  end
   
 end
