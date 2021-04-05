b9e5daac17885fbfaa91b3353110d058b6df37bc
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-05 18:27:18 +0530

    Add iex_client file

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,4 @@
+IEX::Api.configure do |config|
+  config.publishable_token = ENV['Tpk_bfb60b20dbb7410f8394192156639402']
+  config.endpoint = 'https://sandbox.iexapis.com/v1' # use sandbox environment
+end
