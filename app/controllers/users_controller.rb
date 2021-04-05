09baee61f9e4bab8c7c9c192fe3570630c283371
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-05 15:52:33 +0530

    Search friends Ajax submission and JS response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,22 @@ class UsersController < ApplicationController
   end
 
   def search
-  	if params[:friend].present?
-      @friend = friend.new_lookup(params[:friend])
+  	 if params[:friend].present?
+      @friend = params[:friend]
       if @friend
         respond_to do |format|
-          format.js { render partial: 'users/result' }
+          format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "Please enter a valid symbol to search"
-          format.js { render partial: 'users/result' }
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
         end
       end    
     else
-       respond_to do |format|
-        flash.now[:alert] = "Please enter a symbol to search"
-        format.js { render partial: 'users/result' }
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
       end
     end
   end
