261db6157cbe055c09e67ffd35cd2a4e47e50522
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-06 09:26:24 +0530

    add using ENV variables insted of rails credentials when available

--- config/initializers/iex_client.rb
+++
@@ -1,4 +0,0 @@
-IEX::Api.configure do |config|
-  config.publishable_token = ENV['Tpk_bfb60b20dbb7410f8394192156639402']
-  config.endpoint = 'https://sandbox.iexapis.com/v1' # use sandbox environment
-end
